83cfa606978f2ae5183ad20e3c62f2427ee96352
tepavcevic <djordje.tepa@gmail.com>
2023-03-14 16:52:14 +0100

    Adding interactivity added toggle on click to expand/contract form in home page

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,10 +1,22 @@
+import "./styles.css";
 import Form from "../../components/form/Index";
+import List from "../../components/list/Index";
+import { useState } from "react";
 
 export default function Home() {
+  const [formOpened, setFormOpened] = useState(false);
+
+  const handleFormOpened = () => {
+    setFormOpened(!formOpened);
+  };
   return (
     <>
       <h1>Welcome to homepage</h1>
-      <Form />
+      <button className="btn" onClick={handleFormOpened}>
+        {formOpened ? "Close form" : "Add your billing address"}
+      </button>
+      <Form formOpened={formOpened} />
+      <List />
     </>
   );
 }
